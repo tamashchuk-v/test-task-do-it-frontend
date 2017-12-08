1175642b0dd3fb136e19d0449505c89610eea57a
Vladislav Tamashchuk <tamashchuk.v@gmail.com>
2017-12-08 11:34:27 +0200

    button tumbler

--- source/modules/aside/buttons/buttons.js
+++ source/modules/aside/buttons/buttons.js
@@ -1,1 +1,8 @@
-console.log();
+let areaButton = $('.area-button');
+areaButton.click(function () {
+    if (areaButton.hasClass('area-button-active')) {
+        areaButton.removeClass('area-button-active')
+    } else {
+        areaButton.addClass('area-button-active')
+    }
+});
