94a5e82610ab496f4911ecad85461595697ceebd
Vladislav Tamashchuk <tamashchuk.v@gmail.com>
2017-12-08 19:00:37 +0200

    sasha gray

--- source/modules/aside/buttons/buttons.js
+++ source/modules/aside/buttons/buttons.js
@@ -1,4 +1,4 @@
-let areaButton = $('.area-button');
+var areaButton = $('.area-button');
 areaButton.click(function () {
     if (areaButton.hasClass('area-button-active')) {
         areaButton.removeClass('area-button-active')
@@ -6,3 +6,39 @@ areaButton.click(function () {
         areaButton.addClass('area-button-active')
     }
 });
+
+
+
+(function() {
+    $("#range").slider({
+        range: "min",
+        min: 0,
+        max: 100,
+        value: 30,
+    });
+}).call(this);
+
+
+$(function() {
+    $("#range2").slider({
+        range: true,
+        min: 0,
+        max: 500,
+        values: [ 75, 300 ],
+        slide: function( event, ui ) {
+            $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+        }
+    });
+    $( "#amount" ).val( "$" + $( "#range2" ).slider( "values", 0 ) +
+        " - $" + $( "#range2" ).slider( "values", 1 ) );
+});
+
+
+// (function() {
+//     $("#range2").slider({
+//         range: "min",
+//         min: 0,
+//         max: 100,
+//         values: [ 10, 25 ]
+//     });
+// }).call(this);
--- source/modules/header/nav/nav.js
+++ source/modules/header/nav/nav.js
@@ -1,9 +1,9 @@
-let buttonMob = document.getElementById('button-mob-menu');
-let menu = document.getElementsByClassName('navigation')[0];
+var buttonMob = document.getElementById('button-mob-menu');
+var menu = document.getElementsByClassName('navigation')[0];
 buttonMob.onclick = function () {
-    if (menu.style.display === "none") {
-        menu.style.display = "block";
-    } else {
+    if (menu.style.display === "block") {
         menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
     }
 };
