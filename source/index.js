9421ce770eceb294f5efd57e5b7e4af9a43bd13e
Vladislav Tamashchuk <tamashchuk.v@gmail.com>
2017-12-08 11:08:01 +0200

    go

--- source/index.js
+++ source/index.js
@@ -2,3 +2,5 @@ import 'normalize.css';
 import './index.scss';
 import 'font-awesome/css/font-awesome.css';
 import './modules/header/nav/nav';
+import './modules/aside/sub-menu/sub-menu';
+import './modules/aside/buttons/buttons';
---
+++ source/modules/aside/sub-menu/sub-menu.js
@@ -0,0 +1,9 @@
+$('#aside-item__dropdown-1').click(function () {
+    $('#drop_items-1').toggle('fast');
+});
+$('#aside-item__dropdown-2').click(function () {
+    $('#drop_items-2').toggle('fast');
+});
+$('#aside-item__dropdown-3').click(function () {
+    $('#drop_items-3').toggle('fast');
+});
